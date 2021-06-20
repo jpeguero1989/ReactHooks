ef98f4dad4f8dc252056f8352578bf34e86994ee
Jhoan <jpeguero1989@gmail.com>
2021-06-20 15:00:23 -0400

    first

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterYearChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filterExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
+  return (
+    <li>
+      
+      <Card className="expenses">
+       
+        <ExpensesFilter
+          selected={filteredYear}
+          onSelectedYear={filterYearChangeHandler}
+        />
+        <ExpensesChart expenses = {filterExpenses}/>
+        <ExpensesList expenses={filterExpenses}/>
+      </Card>
+    </li>
+  );
+};
+
+export default Expenses;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    console.log("Expense form!");
+    console.log(enteredExpenseData.date);
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+
+    props.onAddExpense(expenseData);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const endEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onEndEditing={endEditingHandler}
+          onSaveExpenseData={saveExpenseDataHandler}
+        />
+      )}
+    </div>
+  );
+};
+export default NewExpense;
